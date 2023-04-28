1808c58e8795d72c4b4baf3f77f3485f424a5c35
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-04-27 23:29:10 -0300

    cam

--- api/src/models/Productos.js
+++ api/src/models/Productos.js
@@ -3,10 +3,39 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('proyecto', {
+  sequelize.define('products', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    details: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    linea:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    marca:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   });
 };
