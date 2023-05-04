c05072d4e410bf2405bd2bc95b72673020391aee
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-05-04 19:20:32 -0300

    hice funcion controladora y ruta de line

---
+++ api/src/routes/getLine.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const { Line } = require("../db");
+const { api } = require("./controllers/infoApis")
+
+const router = Router();
+
+
+router.get('/line', async (req, res) => {
+    try {
+      await api();
+      const lines = await Line.findAll()
+      res.status(200).send(lines)
+    } catch (error) {
+      res.status(400).send(error.message)
+    }
+  })
+
+
+module.exports = router;
