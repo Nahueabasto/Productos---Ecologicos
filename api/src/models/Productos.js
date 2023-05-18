422ada159d53157df8bdbee1773ed7e9d9c70ae3
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-05-18 11:59:51 -0300

    cambios en images

--- api/src/models/Productos.js
+++ api/src/models/Productos.js
@@ -13,10 +13,10 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // images: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    // },
+    images: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     price: {
       type: DataTypes.STRING,
       allowNull: false,
