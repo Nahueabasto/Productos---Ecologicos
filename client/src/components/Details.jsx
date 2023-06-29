53bd986bb6fd5b00a21054bd3c81de19c4593a20
bedettina <bforbedetti@gmail.com>
2023-06-29 16:22:48 -0300

    Fixed photos - now they are shown!

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -9,30 +9,39 @@ export default function ProductDetail(){
     const { id } = useParams()
     const details = useSelector((state) => state.detail);
     const dispatch = useDispatch();
+    let imageUrls = [];
 
     useEffect( () => { 
         dispatch(getDetail(id));
     },[dispatch, id]);
 
-    console.log(details); 
+    if (details && details.images) {
+      imageUrls = details.images.split(",");
+      imageUrls = imageUrls.map((img) => img.trim());
+    }
+
+    console.log(imageUrls); 
 
     return(
-        <div className="contenedor-principal">
-        {details && (
-          <div className="contenedor">
-            <div className="card-imagen">
-              <img src={details.images} alt="Not found" />
-            </div>
-            <div className="detalle">
-              <p className="detalle-texto">Name: {details.name}</p>
-              <p className="detalle-texto">Price: {details.price}</p>
-              <p className="detalle-texto">Stock: {details.stock}</p>
-              <p className="detalle-texto">Detail: {details.details}</p>
-              <p className="detalle-texto">Line: {details.line}</p>
-              
-            </div>
+      <div className="contenedor-principal">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet"></link>
+      {details && (
+        <div className="contenedor">
+          <div className="card-imagen">
+          {imageUrls.map( img => (
+            <img src={img.trim()} alt="Not found" key={img} width="250" height="250"/>
+            ))}
           </div>
-        )}
-      </div>
+          <div className="detalle">
+            <p className="detalle-texto">Name: {details.name}</p>
+            <p className="detalle-texto">Price: {details.price}</p>
+            <p className="detalle-texto">Stock: {details.stock}</p>
+            <p className="detalle-texto">Details: {details.details}</p>
+            <p className="detalle-texto">Line: {details.line}</p>
+            
+          </div>
+        </div>
+      )}
+    </div>
       )
 }
