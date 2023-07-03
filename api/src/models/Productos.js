b46546e49542e9a9e943c169039f913a4710e6e6
bedettina <bforbedetti@gmail.com>
2023-07-03 19:34:07 -0300

    controller and route Products updated - relational table working

--- api/src/models/Productos.js
+++ api/src/models/Productos.js
@@ -3,32 +3,35 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('products', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    images: {
-      type: DataTypes.STRING(1000),
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    stock: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    details: {
-      type: DataTypes.STRING(10000),
-      allowNull: false,
-    },
-
-  });
-};
+    sequelize.define('products', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      images: {
+        type: DataTypes.STRING(1000),
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      details: {
+        type: DataTypes.STRING(10000),
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      }
+    });
+  };
